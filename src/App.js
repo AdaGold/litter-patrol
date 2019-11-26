9d27262313eea48974292a050e8ba4919c484626
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-11-25 16:08:06 -0800

    Added types and click event handling

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       flower: 5,
       mushroom: 5,
     },
-    spawnRate: 1.2, // Hz
+    spawnRate: 0.5, // Hz
     spawnRateRnd: 1.79, // randomization factor
     spawnHeight: 100, // height of item spawn area in pixels
     spawnFloor: 0, // offset from bottom of game "level" in pixels
@@ -30,8 +30,8 @@ class App extends Component {
     };
 
     // Uncomment this to spawn a single test item
-    // const testItem = this.spawnItem(Date.now());
-    // this.state.items.push(testItem);
+    const testItem = this.spawnItem(Date.now());
+    this.state.items.push(testItem);
 
     // Uncomment this to automatically spawn new items
     this.enableSpawner();
@@ -46,12 +46,12 @@ class App extends Component {
   render() {
     const items = this.state.items.map((item, i) => {
       return <GameItem
-               height={item.height}     // Height - used for a CSS style to position on the screen
-               layer={100 + i}          // Layer - used for a CSS style to show items on-top of bg
-               key={item.id}            // Key - to help React with performance
-
+        height={item.height}     // Height - used for a CSS style to position on the screen
+        layer={100 + i}          // Layer - used for a CSS style to show items on-top of bg
+        key={item.id}            // Key - to help React with performance
+        type={item.type}
                // Additional props (event callbacks, etc.) can be passed here
-             />;
+            />;
     });
 
     return (
--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -9,8 +9,18 @@ class GameItem extends Component {
     layer: PropTypes.number.isRequired,
   }
 
-  onItemClicked = () => {
+  onItemClicked = (event) => {
     // Fill this in for Wave 2!
+    // KK: I don't need to account for when an item has already been clicked. It's not gonna change again or add another class to the class list. 
+    const item = event.target;
+    
+    if (this.props.type === 'litter') {
+      item.classList.add('spotted-litter');
+      // add 1 to score
+    } else {
+      item.classList.add('spotted-nature');
+      // remove 1 from score
+    }
   }
     
   render() {
@@ -19,12 +29,12 @@ class GameItem extends Component {
       zIndex: this.props.layer, // use props.layer to set z-index, so we display ontop of background
     };
 
-    // Update this to select the correct icon for each item
-    const icon = ItemIcons.rock;
-
+    // KK: it's getting the correct icon i think?
+    const icon = ItemIcons[this.props.type];
+    
     return (
-      <div className="game-item" style={itemStyle}>
-        <img src={icon} alt="Item" className="icon-item"></img>
+      <div className="game-item" style={itemStyle} onClick={this.onItemClicked}>
+        <img src={icon} alt={this.props.type} className="icon-item"></img>
       </div>
     );
   }
