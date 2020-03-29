66df441369227f3486ea5102d2b9b175929ebab1
CheezItMan <chris@adadevelopersacademy.org>
2020-03-28 17:31:22 -0700

    move GameItem to functional component

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -3,31 +3,33 @@ import '../App.css';
 import ItemIcons from '../ItemIcons.js';
 import PropTypes from 'prop-types';
 
-class GameItem extends Component {
-  static propTypes = {
-    height: PropTypes.number.isRequired,
-    layer: PropTypes.number.isRequired,
-  }
+const GameItem = (props) => {
 
-  onItemClicked = () => {
+  const onItemClicked = () => {
     // Fill this in for Wave 2!
-  }
-    
-  render() {
-    const itemStyle = {
-      bottom: `${this.props.height}px`, // use props.height to offset from the bottom of screen
-      zIndex: this.props.layer, // use props.layer to set z-index, so we display ontop of background
-    };
 
-    // Update this to select the correct icon for each item
-    const icon = ItemIcons.rock;
-
-    return (
-      <div className="game-item" style={itemStyle}>
-        <img src={icon} alt="Item" className="icon-item"></img>
-      </div>
-    );
   }
+  const itemStyle = {
+    bottom: `${ props.height }px`, // use props.height to offset from the bottom of screen
+    zIndex: props.layer, // use props.layer to set z-index, so we display ontop of background
+  };
+
+  // Update this to select the correct icon for each item
+  const icon = ItemIcons['rock'];
+
+  return (
+    <div className="game-item" style={itemStyle}>
+      <img src={icon} alt="Item" className="icon-item"></img>
+    </div>
+  );
 }
+
+GameItem.propTypes = {
+  height: PropTypes.number.isRequired,
+  layer: PropTypes.number.isRequired,
+  // More props are probably needed
+
+}
+
 
 export default GameItem;
